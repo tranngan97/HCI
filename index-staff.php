6028420339184dcae4dd53a0a650b4be02e846d9
dungtv0805 <dungtv0805@gmail.com>
2019-12-17 07:30:35 +0700

    chỉnh sửa README.md

--- index-staff.php
+++ index-staff.php
@@ -260,7 +260,7 @@
           </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-3">
-          <div class="card text-white bg-success o-hidden h-100">
+          <!-- <div class="card text-white bg-success o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
                 <i class="fas fa-fw fa-shopping-cart"></i>
@@ -273,10 +273,10 @@
                 <i class="fas fa-angle-right"></i>
               </span>
             </a>
-          </div>
+          </div> -->
         </div>
         <div class="col-xl-3 col-sm-6 mb-3">
-          <div class="card text-white bg-danger o-hidden h-100">
+          <!-- <div class="card text-white bg-danger o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
                 <i class="fas fa-fw fa-life-ring"></i>
@@ -289,7 +289,7 @@
                 <i class="fas fa-angle-right"></i>
               </span>
             </a>
-          </div>
+          </div> -->
         </div>
       </div>
       <ol class="breadcrumb">
